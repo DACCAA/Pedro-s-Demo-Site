9e502bad5d200a7cdbfe418ebf6c79ff8f7dd8c1
Danny Franklin <dannyfranklin99@gmail.com>
2019-09-04 16:42:23 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Perdo's Pizza | The Finest Italian in Walsall</title>
+  <link rel="stylesheet" type="text/css" href="https://daccaa.com/UI/daccaa-ui.css">
+  <link rel="stylesheet" type="text/css" href="main.css?v=<?php echo filemtime('main.css'); ?>">
+</head>
+<body>
+
+  <div class="menu-bar">
+    <div class="inner">
+      <div class="left">
+        <img src="logo-white.png" alt="Pedro's Pizza Logo" />
+      </div>
+      <div class="right">
+        <a href="./" class="selected">Home</a><a href="about">About</a><a href="menu">Menu</a><a href="contact">Contact</a>
+      </div>
+    </div>
+  </div>
+  
+  <div class="sale">
+    <div class="inner">
+      Mid-week madness, get two for the price of one on all pizza!
+    </div>
+  </div>
+  
+  <div class="image-holder">
+    <div class="overlay centre">
+      <div class="inner-holder">
+        <div class="text">
+          Finest Italian in Walsall
+        </div>
+        <div class="button">
+          <a href="javascript:void(0);" class="reserve">Reserve a seat</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+  <script src="main.js?v=<?php echo filemtime('main.js'); ?>"></script>  
+</body>
+</html>
