35a712a97dd6f3470e2b3685ee97378c25aa2cb9
Danny Franklin <dannyfranklin99@gmail.com>
2019-09-09 00:09:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
-  <title>Perdo's Pizza | The Finest Italian in Walsall</title>
+  <title>Pedro's Pizza | The Finest Italian in Walsall</title>
   <link rel="stylesheet" type="text/css" href="https://daccaa.com/UI/daccaa-ui.css">
   <link rel="stylesheet" type="text/css" href="main.css?v=<?php echo filemtime('main.css'); ?>">
 </head>
@@ -39,7 +39,28 @@
     </div>
   </div>
   
+  <div class="content">
+    <div class="inner">
+      <div class="title">
+        Welcome to Pedro's Pizza
+      </div>
+      <div class="desc">
+        We have the finest pizza in Walsall. People travel from all around the globe to taste Pedro's Pizza. Join the trend of people and come to Walsall and stop by at Pedro's to experience the difference. Our customers love Pedro's Pizza and with 100% return rate - you know what to do...
+      </div>
+      
+      <img src="https://daccaa.com/tutorial/pedro-final/pedro.jpg" alt="A picture of Pedro" class="pedro" />
+      
+      <div class="desc">
+        Hi, I am Pedro, I am the guy making these award winning pizzas. I worked tirelessly with my Pa to make these award winning pizzas. It all stems from my traditional Italian roots whereby my Great Grandad was a master cook renown across the whole of Italy. My Pa and I created this authentic Italian menu to conjure up the best experience for all of our beloved customers.
+      </div>
+      
+    </div>
+  </div>
+  
+  <div id="map"></div> 
+  
   <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+  <script src="https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.js"></script>
   <script src="main.js?v=<?php echo filemtime('main.js'); ?>"></script>  
 </body>
 </html>
